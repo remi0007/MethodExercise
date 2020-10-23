ddc26967f2c0e14ba4c3b2e7bb3f97ff59bd161f
Remi Douah <remidouah@gmail.com>
2020-10-23 11:09:44 -0500

    Favorite color and music

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // -----------------Exercise 1
+
+            // Name: Michael
+            // Favorite Color: Blue
+            // Favorite Animal: Walrus
+            // Favorite Band: The Beatles
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {unserName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($" {color} is an awsome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
